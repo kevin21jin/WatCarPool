d781bb59a6df1369cf4f67046c9872a96f764e2f
kevin21jin <kevinjin0221@gmail.com>
2022-10-12 21:52:10 -0400

    create sql script for creating tables

--- server/database/sql/createtables.sql
+++ server/database/sql/createtables.sql
@@ -1,16 +1,60 @@
-
-
-CREATE TABLE IF NOT EXISTS User (
-    userId INTEGER NOT NULL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(60),
-    phone VARCHAR(60),
+CREATE TABLE IF NOT EXISTS User
+(
+    userID INT(11) NOT NULL,
+    username VARCHAR(25) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    phone VARCHAR(60) NOT NULL,
     type VARCHAR(100) NOT NULL,
-    joinedAt DATE NOT NULL
+    joinedAt DATE NOT NULL,
+    PRIMARY KEY (userID)
 );
 
+CREATE TABLE IF NOT EXISTS Passenger
+(
+    userID INT(11) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES User(userID),
+    PRIMARY KEY (userID)
+);
 
-CREATE TABLE IF NOT EXISTS Passenger (
-    
+CREATE TABLE IF NOT EXISTS Driver
+(
+    userID INT(11) NOT NULL,
+    rating DECIMAL(1,2),
+    FOREIGN KEY (userID) REFERENCES User(userID),
+    PRIMARY KEY (userID)
+);
+
+CREATE TABLE IF NOT EXISTS Vehicle
+(
+    vehicleID INT(11) NOT NULL,
+    driverID INT(11) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    capacity INT(2) NOT NULL,
+    FOREIGN KEY (driverID) REFERENCES Driver(userID),
+    PRIMARY KEY (vehicleID)
+);
+
+CREATE TABLE IF NOT EXISTS Trip
+(
+    tripID INT(11) NOT NULL,
+    driverID INT(11) NOT NULL,
+    vehicleID INT(11) NOT NULL, 
+    origin VARCHAR(25) NOT NULL,
+    destination VARCHAR(25) NOT NULL,
+    departTime DATETIME NOT NULL,
+    desciption VARCHAR(200),
+    FOREIGN KEY (driverID) REFERENCES Driver(userID),
+    FOREIGN KEY (vehicleID) REFERENCES Vehicle(vehicleID),
+    PRIMARY KEY (tripID)
+);
+
+CREATE TABLE IF NOT EXISTS Travelled
+(
+    tripID INT(11) NOT NULL,
+    passengerID INT(11) NOT NULL,
+    rating DECIMAL(1,1),
+    FOREIGN KEY (tripID) REFERENCES Trip(tripID),
+    FOREIGN KEY (passengerID) REFERENCES Passenger(userID), 
+    PRIMARY KEY (tripID, passengerID)
 )
