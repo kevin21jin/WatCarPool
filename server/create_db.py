389889d64e1cac45daa249793324c5fdd3572297
Fakevin0613 <ke_qinyang@outlook.com>
2022-10-14 12:36:54 -0400

    enable database create

--- server/create_db.py
+++ server/create_db.py
@@ -3,15 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwrd="12345678",
+    password="12345678",
 )
 
 my_cursor = mydb.cursor()
 
 # Create Database
-# my_cursor.execute("CREATE DATABASE WCP_DB")
-my_cursor.execute("CREATE TABLE()")
-my_cursor.execute("INSERT INTO")
+my_cursor.execute("CREATE DATABASE WCP_DB")
+# my_cursor.execute("CREATE TABLE()")
+# my_cursor.execute("INSERT INTO")
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
