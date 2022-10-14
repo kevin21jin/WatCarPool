c60f124f21a4da09ef270d4a1af35b6d51d922a6
Chengyu Jin <brucejin0801@gmail.com>
2022-10-14 15:02:30 -0400

    login page

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -5,16 +5,17 @@ import {Container} from 'react-bootstrap'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { Aboutus } from './pages/Aboutus';
 function App() {
     return (
         <BrowserRouter>
-        
         <Header/>
         <Container>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/aboutus" element={<Aboutus />} />
         </Routes>
         </Container>
         </BrowserRouter>
---
+++ client/my-app/src/pages/Aboutus.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Aboutus = () => {
+  return (
+    <div>About us</div>
+  )
+}
