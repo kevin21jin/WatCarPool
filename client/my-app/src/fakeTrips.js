58416c2f58c7b605ad0b63242bfb85bbbab0feca
Fakevin0613 <ke_qinyang@outlook.com>
2022-10-15 23:18:51 -0400

    add trip card

--- client/my-app/src/fakeTrips.js
+++ client/my-app/src/fakeTrips.js
@@ -3,40 +3,46 @@ const fakeTrips = [
         from: "from1",
         to: "to1",
         dapartTime: "departime1",
-        price: 10.00,
+        price: 1.00,
         model: "model1",
         capacity: "capacity1",
+        description: "description long long long long long long long long long long long long"
     },
     {
         from: "from2",
         to: "to2",
         dapartTime: "departime2",
-        price: 10.00,
+        price: 2.00,
         model: "model2",
         capacity: "capacity2",
+        description: "description long long"
     },
     {
         from: "from3",
         to: "to3",
         dapartTime: "departime3",
-        price: 10.00,
+        price: 333.00,
         model: "model3",
         capacity: "capacity3",
+        description: "description small"
     },
     {
         from: "from4",
         to: "to4",
         dapartTime: "departime4",
-        price: 10.00,
+        price: 144.00,
         model: "model4",
         capacity: "capacity4",
+        description: "description nothing"
     },
     {
         from: "from5",
         to: "to5",
         dapartTime: "departime5",
-        price: 10.00,
+        price: 6.00,
         model: "model5",
         capacity: "capacity5",
     }
 ]
+
+export default fakeTrips
