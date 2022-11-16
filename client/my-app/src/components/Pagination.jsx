0df2505d7eb94c451b8a7d87d92c9fc68db07611
Chengyu Jin <brucejin0801@gmail.com>
2022-11-16 15:29:39 -0500

    pagination bug fix

--- client/my-app/src/components/Pagination.jsx
+++ client/my-app/src/components/Pagination.jsx
@@ -7,19 +7,22 @@ export const Pagination = ({curPage, postPerpage, totalPage, paginate, setCurPag
     const [min, setmin] = useState(0);
     const [limit, setlimit] = useState(5);
     const handlenxt =()=>{
+        
         setCurPage(curPage +1);
-        if((curPage + 1)%limit==0){
+        if((curPage + 1) > max){
             setmax(max + limit);
             setmin(min + limit);
          }
     };
 
     const handlepre =()=>{
+        if(curPage > 1){
         setCurPage(curPage -1);
         if((curPage - 1)%limit==0){
             setmax(max - limit);
             setmin(min - limit);
          }
+        }
     };
 
     for(let i = 1; i <= Math.ceil(totalPage/ postPerpage); i++){
@@ -28,7 +31,7 @@ export const Pagination = ({curPage, postPerpage, totalPage, paginate, setCurPag
     return(
             <div className='Container' >
             <ul className='Pagination'>
-                <li><button onClick={handlepre}>prev</button></li>
+                <li><p onClick={handlepre}>{'<'}</p></li>
                 {pageNumber.map((number) => {
                     if(number < max+1 && number > min){
                         return(
@@ -41,7 +44,7 @@ export const Pagination = ({curPage, postPerpage, totalPage, paginate, setCurPag
                         return null;
                     }
                     })}
-                    <li><button onClick={handlenxt}>next</button></li>
+                    <li><p onClick={handlenxt}> {'>'} </p></li>
             </ul>
             </div>
     )
