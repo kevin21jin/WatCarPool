eabfb7cf429367f400c9eafa0e4f09f0cffbfcc0
kevin21jin <kevinjin0221@gmail.com>
2022-11-27 17:31:37 -0500

    fix modal bug

--- client/my-app/src/components/Rating.jsx
+++ client/my-app/src/components/Rating.jsx
@@ -1,102 +1,104 @@
 import React from "react";
 import { FaStar } from "react-icons/fa"
 import { useState } from 'react';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import axios from 'axios'
 import { rateRoute } from '../api/ApiRoutes'
 import { toast } from "react-toastify"
-const Modal = ({open,onClose, trip, currentUser}) =>{
-    const stars = Array(5).fill(0);
-    const [currentValue, setcurrentValue] = useState(false);
-    const [hoverValue, sethoverValue] = useState(false);
-    const [rating, setRating] = useState(0)
-    const toastOptions = {
-        position: "bottom-right",
-        autoClose: 5000,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "light"
-      };
-    const handleClick = value =>{
-        setcurrentValue(value)
-        setRating(value)
-    };
-    const handleMouseOver = value =>{
-        sethoverValue(value)
-    }
-    const handleMouseLeave = value =>{
-        sethoverValue(undefined)
-    }
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const requestJson = {
-            driverID: trip.driverID,
-            vehicleID: trip.vehicleID,
-            tripID: trip.tripID,
-            passengerID: currentUser.userId,
-            rating: rating
-          }
-          const { data } = await axios.put(rateRoute, requestJson);
-          if (data.status === "Fail") {
-            toast.error(data.errorMessage, toastOptions);
-          }else if (data.status === "Success") {
-            console.log("success")
-            console.log(data)
-            onClose={onClose}
-          }
-    }
 
-    const color = {
-        orange: "#ffba5a",
-        grey: "#a9a9a9"
+const Modal = ({ open, onClose, trip, currentUser, index }) => {
+  const stars = Array(5).fill(0);
+  // console.log(trip)
+  const [currentValue, setcurrentValue] = useState(false);
+  const [hoverValue, sethoverValue] = useState(false);
+  const [rating, setRating] = useState(0)
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 5000,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "light"
+  };
+  const handleClick = value => {
+    setcurrentValue(value)
+    setRating(value)
+  };
+  const handleMouseOver = value => {
+    sethoverValue(value)
+  }
+  const handleMouseLeave = value => {
+    sethoverValue(undefined)
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const requestJson = {
+      driverID: trip.driverID,
+      vehicleID: trip.vehicleID,
+      tripID: trip.tripID,
+      passengerID: currentUser.userId,
+      rating: rating
     }
-  if(!open) return null
-  return(
+    const { data } = await axios.put(rateRoute, requestJson);
+    if (data.status === "Fail") {
+      toast.error(data.errorMessage, toastOptions);
+    } else if (data.status === "Success") {
+      console.log("success")
+      console.log(data)
+      console.log(requestJson)
+      onClose = { onClose }
+    }
+  }
+
+  const color = {
+    orange: "#ffba5a",
+    grey: "#a9a9a9"
+  }
+  if (open != index) return null
+  return (
     <div className="overlay">
       <div className="modalContainer">
         <div className="modalright">
-        <div className="modalcontent">
-        <button button onClick={onClose} className="closebtn"><i class="fa fa-times-circle"/></button> 
+          <div className="modalcontent">
+            <button onClick={onClose} className="closebtn"><i className="fa fa-times-circle" /></button>
             <h2 align="center">Rating for this trip</h2>
             <p></p><p></p><p></p>
             <div align="center">
-                {
-                    stars.map((_, index) => {
-                        return(
-                            <FaStar
-                            key={index}
-                            size={56}
-                            style= {{
-                                marginRight: 10,
-                                cursor: "pointer"
-                            }}
-                            color={(hoverValue||currentValue) > index ? color.orange: color.grey}
-                            onClick={()=> handleClick(index+1)}
-                            onMouseOver={()=>handleMouseOver(index+1)}
-                            onMouseLeave = {handleMouseLeave}
-                            />
-                        )
-                    })
-                }
-                <></>
+              {
+                stars.map((_, index) => {
+                  return (
+                    <FaStar
+                      key={index}
+                      size={56}
+                      style={{
+                        marginRight: 10,
+                        cursor: "pointer"
+                      }}
+                      color={(hoverValue || currentValue) > index ? color.orange : color.grey}
+                      onClick={() => handleClick(index + 1)}
+                      onMouseOver={() => handleMouseOver(index + 1)}
+                      onMouseLeave={handleMouseLeave}
+                    />
+                  )
+                })
+              }
+              <></>
             </div>
             <div align="center">
-            <p></p>
-            <textarea 
+              <p></p>
+              <textarea
+                style={{ resize: "none", height: "250px", width: "80%" }}
                 placeholder="Please give us your feedback!"
-                rows={10}
-                cols = {50}
-                maxLength = {300}
-                />
-                </div>
+                maxLength={1000}
+              />
+            </div>
+          </div>
+          <div align="center">
+            <Button type='submit' onClick={(e) => handleSubmit(e)}>Submit</Button>
+          </div>
         </div>
-        <div align = "center">
-        <Button  type='submit' onClick={(e) => handleSubmit(e)}>Submit</Button>
-        </div>
-        </div>
-        </div>
-        </div>
+      </div>
+    </div>
   )
 
 }
