7d779d48333bce1af1e3a32ab39bffedb93df36a
Chengyu Jin <brucejin0801@gmail.com>
2022-11-26 22:18:24 -0500

    npm install icons

--- client/my-app/src/components/Rating.jsx
+++ client/my-app/src/components/Rating.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import { Login } from '../pages/Login';
+import {Fastar} from "react-icons/fa"
 const Modal = ({open,onClose}) =>{
+    const color = {
+        orange: "#ffba5a",
+        grey: "#a9a9a9"
+    }
   if(!open) return null
   return(
     <div className="overlay">
       <div className="modalContainer">
         <div className="modalright">
-        
         <div className="modalcontent">
         <button button onClick={onClose} className="closebtn">x</button> 
-        
+        <div>
+            <h2 align="center">Rating for this trip</h2>
+        </div>
         </div>
         </div>
         </div>
     </div>
   )
+
+  const style = {
+    container: {
+        display : "flex",
+        flexDirection: "column",
+        alignItems: "center"
+
+    }
+  }
 }
 
 export default Modal
