38b85207c9ec14357b05f1ca3283c5f5519c4efe
kevin21jin <kevinjin0221@gmail.com>
2022-11-29 00:43:17 -0500

    not found page

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AddTrip } from './pages/AddTrip';
 import { Home } from './pages/Home'
-import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Welcome } from './pages/Welcome';
 import { AddVehicle } from './pages/AddVehicle';
 import { SearchTrip } from './pages/SearchTrip';
 import { Account } from './pages/Account'
 import { SearchTripResult } from './pages/SearchTripResult';
+import { NotFound } from './components/NotFound'
 
 function App() {
     return (
@@ -16,13 +16,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/addtrip" element={<AddTrip />} />
           <Route path="/addvehicle" element={<AddVehicle />} />
           <Route path="/search" element={<SearchTrip />} />
           <Route path="/searchresult" element={<SearchTripResult />} />
           <Route path="/account" element={<Account />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         </BrowserRouter>
     );
